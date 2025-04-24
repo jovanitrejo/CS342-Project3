b168f6dd333006b07c1975eb5396a00ec4916679
Jovani Trejo <jovanitrejo@icloud.com>
2025-04-23 20:40:55 -0500

    Added new callback for invites

--- Project3Client/src/main/java/Scenes/MainMenu.java
+++ Project3Client/src/main/java/Scenes/MainMenu.java
@@ -1,7 +1,9 @@
 package Scenes;
 
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
@@ -10,13 +12,25 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import utils.CustomJavaFXElementsTools;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class MainMenu {
     private final StackPane root;
     public final VBox onlineUsersList;
+    private final Consumer<String> inviteAcceptedCallback;
+    private final Consumer<String> inviteDeniedCallback;
+    private final Consumer<String> inviteUserCallback;
+    private final HashMap<String, Button> availableUsers = new HashMap<>();
+    private final ArrayList<VBox> activeInvitePopUps = new ArrayList<>();
+    private final ArrayList<String> pendingInvites = new ArrayList<>();
 
-    public MainMenu(Consumer<Boolean> findNewGameCallback, Consumer<Boolean> changeUsernameCallback) {
+    public MainMenu(Consumer<Boolean> findNewGameCallback, Consumer<Boolean> changeUsernameCallback, Consumer<String> inviteAcceptedCallback, Consumer<String> inviteDeniedCallback, Consumer<String> inviteUserCallback) {
+        this.inviteAcceptedCallback = inviteAcceptedCallback;
+        this.inviteDeniedCallback = inviteDeniedCallback;
+        this.inviteUserCallback = inviteUserCallback;
         final VBox welcomeText = CustomJavaFXElementsTools.createCustomLabel("CS342 Connect 4");
 
         welcomeText.setAlignment(Pos.TOP_CENTER);
@@ -53,6 +67,109 @@ public class MainMenu {
         StackPane.setAlignment(welcomeText, Pos.TOP_CENTER);
         StackPane.setAlignment(options, Pos.CENTER);
         StackPane.setAlignment(fullWindow, Pos.CENTER_RIGHT);
+    }
+
+    public void showInvitePopup(String requestorUsername) {
+        final VBox[] newInvitePopUp = new VBox[1];
+        pendingInvites.add(requestorUsername);
+
+        newInvitePopUp[0] = CustomJavaFXElementsTools.createPopUp(
+                () -> {
+                    // Case where user invite is denied
+                    inviteDeniedCallback.accept(requestorUsername);
+                    hideReplayPopUp(newInvitePopUp[0]);
+                    pendingInvites.remove(requestorUsername);
+                },
+                () -> {
+                    // Case where user invite is accepted
+                    inviteAcceptedCallback.accept(requestorUsername);
+                    hideReplayPopUp(newInvitePopUp[0]);
+                    pendingInvites.remove(requestorUsername);
+                    activeInvitePopUps.remove(newInvitePopUp[0]);
+                    hideAllOtherInvitePopUps();
+
+                    // Reject all other invites since we have accepted one
+                    for(String invite : pendingInvites) {
+                        inviteDeniedCallback.accept(invite);
+                    }
+                },
+                requestorUsername + " has requested to play against you! Do you accept?",
+                "No",
+                "Yes"
+        );
+        activeInvitePopUps.add(newInvitePopUp[0]);
+        root.getChildren().add(newInvitePopUp[0]);
+    }
+
+    private void hideAllOtherInvitePopUps() {
+        for (VBox popup : new ArrayList<>(activeInvitePopUps)) {
+            root.getChildren().remove(popup);
+        }
+        activeInvitePopUps.clear();
+    }
+
+    private void hideReplayPopUp(VBox newInvitePopUp) {
+        ObservableList<Node> kids = root.getChildren();
+        kids.remove(newInvitePopUp);
+    }
+
+    public void showRequestorIsAlreadyInGame() {
+        final VBox[] newNotification = new VBox[1];
+        newNotification[0] = CustomJavaFXElementsTools.createNotification(
+                () -> hideReplayPopUp(newNotification[0]),
+                "The user you accepted an invite from is already in a game!"
+        );
+        root.getChildren().add(newNotification[0]);
+    }
+
+    public void updateActiveUsers(ArrayList<String> usernames, String myUsername) {
+        // Remove the existing list of users
+        System.out.println("Updating active users");
+        onlineUsersList.getChildren().clear();
+        availableUsers.clear();
+
+        // Add full-list of current users to list
+        for(String username : usernames) {
+            if (!Objects.equals(username, myUsername)) {
+                Text usernameText = new Text(username);
+                usernameText.setFont(Font.font("Londrina Solid", 20));
+                usernameText.setStroke(Color.BLACK);
+                usernameText.setStrokeWidth(1);
+                Button thisButton = CustomJavaFXElementsTools.createStyledButton(60, 25, "#00B2FF", Color.WHITE, "Invite", 16, true);
+                thisButton.setOnAction(e -> {
+                    System.out.println("Sending invite to: " + username);
+                    inviteUserCallback.accept(username);
+                    disableInviteButton(username);
+                });
+                availableUsers.put(username, thisButton);
+                HBox users = new HBox(usernameText, thisButton);
+                onlineUsersList.getChildren().add(users);
+            }
+        }
+        System.out.println("Current available number of users: " + availableUsers.size());
+        for (String username : availableUsers.keySet()) {
+            System.out.println(username);
+        }
+    }
+
+    public void reEnableInviteButton(String username) {
+        System.out.println("Trying to re-enable: " + username);
+        if (!availableUsers.containsKey(username)) {
+            System.out.println("No such user found in map!");
+            return;
+        }
+        Button usersButton = availableUsers.get(username);
+        usersButton.setDisable(false);
+    }
+
+    public void disableInviteButton(String username) {
+        System.out.println("Trying to disable: " + username);
+        if (!availableUsers.containsKey(username)) {
+            System.out.println("No such user found in map!");
+            return;
+        }
+        Button usersButton = availableUsers.get(username);
+        usersButton.setDisable(true);
     }
 
     public StackPane getRoot() {
