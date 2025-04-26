fa515313c1cfdccbbdfba917f8c67fe9841bef9f
Jovani Trejo <jovanitrejo@icloud.com>
2025-04-26 12:48:43 -0500

    Added more logging
    Signed-off-by: Jovani Trejo <jovanitrejo@icloud.com>

--- Project3Server/src/main/java/GuiServer.java
+++ Project3Server/src/main/java/GuiServer.java
@@ -31,6 +31,7 @@ public class GuiServer extends Application{
 		dispatcher.registerHandler(LoginMessage.class, (loginMessage, clientThread) -> {
 			if (Objects.equals(loginMessage.getUsername(), "") || loginMessage.getUsername().contains(" ")) {
 				clientThread.sendMessage(new LoginResponse(false, "Username cannot be empty or contain spaces!"));
+				Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " tried setting their username to " + loginMessage.getUsername() +". but it does not meet requirements. Sending error to client."));
 				return;
 			}
 			for(Server.ClientThread client : serverConnection.clients) {
@@ -38,6 +39,7 @@ public class GuiServer extends Application{
 					if (client == clientThread) {
 						try {
 							clientThread.sendMessage(new LoginResponse(false, "You already set your username to this. Try a new one."));
+							Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " tried setting their username to " + loginMessage.getUsername() +". But it's taken. Sending error to client."));
 							return;
 						} catch (Exception e) {
 							throw new RuntimeException(e);
@@ -65,6 +67,7 @@ public class GuiServer extends Application{
 					Platform.runLater(() -> listItems.getItems().add("The server set client #" + clientThread.count + "'s username to " + loginMessage.getUsername() + " and is now authorized."));
 				}
 				// Send to user a list of people currently in the lobby
+				Platform.runLater(() -> listItems.getItems().add("Notifying " + clientThread.getUsername() + " of other users who can play"));
 				for (String user : serverConnection.lobby) {
 					clientThread.sendMessage(new UpdateLobbyMessage(user, true));
 				}
@@ -78,13 +81,14 @@ public class GuiServer extends Application{
 
 		// Handle NewGameRequests from clients
 		dispatcher.registerHandler(NewGameMessage.class, ((newGameMessage, clientThread) -> {
-			Platform.runLater(() -> listItems.getItems().add("Got a game request! Sending response..."));
+			Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " wants to play a game."));
 			if (!serverConnection.waitingForGame.isEmpty()) {
-				Platform.runLater(() -> listItems.getItems().add("Two players wanna play! Starting a new game..."));
+				Platform.runLater(() -> listItems.getItems().add("There are two people in the queue! Starting a new game..."));
 				// at least one player in the queue
 				Server.ClientThread opponent = serverConnection.waitingForGame.get(0);
 				serverConnection.waitingForGame.remove(opponent);
 				Thread newGame = getThread(opponent, clientThread);
+				Platform.runLater(() -> listItems.getItems().add("Paired " + clientThread.getUsername() + " with " + opponent.getUsername() + ". Now initializing the game..."));
 				synchronized(activeGames) {
 					activeGames.add(newGame);
 				}
@@ -92,7 +96,7 @@ public class GuiServer extends Application{
 				sendToAllAuthorizedUsers(new AvailableUsersMessage(getAvailableUsers()));
 				newGame.start();
 			} else {
-				Platform.runLater(() -> listItems.getItems().add("A player wants to play, need to find an opponent..."));
+				Platform.runLater(() -> listItems.getItems().add("There are no players that are also wanting to play right now. Adding " + clientThread.getUsername() + " to queue..."));
 				serverConnection.waitingForGame.add(clientThread);
 				clientThread.sendMessage(new WaitingInQueueMessage());
 			}
@@ -100,13 +104,19 @@ public class GuiServer extends Application{
 
 		// Handling moves
 		dispatcher.registerHandler(MoveMessage.class, (moveMessage, clientThread) -> {
-			Platform.runLater(() -> listItems.getItems().add("User made a move! Processing"));
+			Platform.runLater(() -> listItems.getItems().add("UPDATE FROM " + clientThread.activeGame.player1.getUsername() + " AND " + clientThread.activeGame.player2.getUsername() + "'S GAME: " + clientThread.getUsername() + " added a new piece to the board."));
 			clientThread.activeGame.onMove(moveMessage, clientThread);
 		});
 
 		// Handling quit
 		dispatcher.registerHandler(QuitMessage.class, ((quitMessage, clientThread) -> {
-			Platform.runLater(() -> listItems.getItems().add("Some one quit the game! Shutting down active game..."));
+			if (clientThread.activeGame == null) {
+				// Client left queue before could join a game, removing from queue
+				Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " left the queue before someone could join them."));
+                serverConnection.waitingForGame.remove(clientThread);
+				return;
+			}
+			Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " quit the game! Shutting down active game session between " + clientThread.activeGame.player1.getUsername() + " and " + clientThread.activeGame.player2.getUsername()));
 			// Remove from the list of active games
 			activeGames.remove(getThread(clientThread.activeGame.player1, clientThread.activeGame.player2));
 			GameSession currentGame = clientThread.activeGame.player1.activeGame;
@@ -126,6 +136,7 @@ public class GuiServer extends Application{
 		dispatcher.registerHandler(ReplayRequest.class, ((replayRequest, clientThread) -> {
 			if (clientThread.activeGame != null) {
 				Server.ClientThread opponent = Objects.equals(clientThread.activeGame.player1.getUsername(), clientThread.getUsername()) ? clientThread.activeGame.player2 : clientThread.activeGame.player1;
+				Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " wants to replay " + opponent.getUsername() + "... Letting them know."));
 				opponent.sendMessage(new ReplayRequest(clientThread.getUsername()));
 			}
 		}));
@@ -135,9 +146,11 @@ public class GuiServer extends Application{
 			if (clientThread.activeGame != null) {
                 Server.ClientThread opponent = Objects.equals(clientThread.activeGame.player1.getUsername(), clientThread.getUsername()) ? clientThread.activeGame.player2 : clientThread.activeGame.player1;
                 if (!replayResponse.isAccepted()) {
+					Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " DENIED " + opponent.getUsername() + "'s request to replay"));
                     opponent.sendMessage(new ReplayResponse(replayResponse.isAccepted()));
 				} else {
 					// End the current game and create a new one for the two users to play.
+					Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " ACCEPTED " + opponent.getUsername() + "'s request to replay"));
 					GameSession currentGame = clientThread.activeGame;
 					if (currentGame == null) return;
 					currentGame.endGame();
@@ -153,6 +166,7 @@ public class GuiServer extends Application{
 		// Handling sending a new chat message
 		dispatcher.registerHandler(ChatMessage.class, ((chatMessage, clientThread) -> {
 			if (clientThread.activeGame == null) return;
+			Platform.runLater(() -> listItems.getItems().add("UPDATE FROM " + clientThread.activeGame.player1.getUsername() + " AND " + clientThread.activeGame.player2.getUsername() + "'S GAME: " + clientThread.getUsername() + " Sent " + chatMessage.getMessage() + " to the chat box."));
 			GameSession activeGame = clientThread.activeGame;
 			activeGame.chat.add(clientThread.getUsername() + ": " + chatMessage.getMessage());
 			Server.ClientThread opponent = clientThread == activeGame.player1 ? activeGame.player2 : activeGame.player1;
@@ -161,6 +175,7 @@ public class GuiServer extends Application{
 
 		// Handling sending invites to users
 		dispatcher.registerHandler(InviteUserMessage.class, ((inviteUserMessage, clientThread) -> {
+			Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " sent an invite to " + inviteUserMessage.getUsername() + " to play a game!"));
 			for (Server.ClientThread client : serverConnection.clients) {
 				if (client.activeGame == null && client.isAuthorized()) {
 					if (Objects.equals(client.getUsername(), inviteUserMessage.getUsername())) {
@@ -175,6 +190,7 @@ public class GuiServer extends Application{
 			for (Server.ClientThread client : serverConnection.clients) {
 				if (client.activeGame == null && client.isAuthorized()) {
 					if (Objects.equals(client.getUsername(), inviteAcceptedMessage.getOpponent())) {
+						Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " ACCEPTED and invite from " + inviteAcceptedMessage.getOpponent()));
 						Thread newGame = getThread(client, clientThread);
 						synchronized(activeGames) {
 							activeGames.add(newGame);
@@ -184,6 +200,7 @@ public class GuiServer extends Application{
 					}
 				} else if (client.activeGame != null && client.isAuthorized()) {
 					if (Objects.equals(client.getUsername(), inviteAcceptedMessage.getOpponent())) {
+						Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " ACCEPTED and invite from " + inviteAcceptedMessage.getOpponent() + " but they are already in a game!"));
 						clientThread.sendMessage(new UserAlreadyInGameMessage());
 						return;
 					}
@@ -193,6 +210,7 @@ public class GuiServer extends Application{
 
 		// Handling when a user denies an invitation
 		dispatcher.registerHandler(InviteDeniedMessage.class, ((inviteDeniedMessage, clientThread) -> {
+			Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " DENIED an invite from " + inviteDeniedMessage.getOpponent() + " to play a game!"));
 			for (Server.ClientThread client : serverConnection.clients) {
 				if (client.activeGame == null && client.isAuthorized()) {
 					if (Objects.equals(client.getUsername(), inviteDeniedMessage.getOpponent())) {
@@ -206,6 +224,7 @@ public class GuiServer extends Application{
 		// Handling when a user joins the lobby
 		dispatcher.registerHandler(UserLobbyUpdateMessage.class, (userLobbyUpdate, clientThread) -> {
 			boolean joinedOrLeftLobby;
+			Platform.runLater(() -> listItems.getItems().add(clientThread.getUsername() + " is " + (userLobbyUpdate.isJoinedLobby() ? "JOINING" : "LEAVING") + " the lobby."));
 			if (userLobbyUpdate.isJoinedLobby()) {
 				serverConnection.lobby.add(clientThread.getUsername());
 				joinedOrLeftLobby = true;
